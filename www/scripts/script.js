27cfee778a81dd2aadafa1273b48aaa11034aafe
Andreev Kirill <andreev_kir96@mail.ru>
2022-03-01 23:42:12 +0300

    1

---
+++ www/scripts/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.nav-tel').onclick = function(){
+  document.querySelector('.line').classList.toggle('active1');
+  document.querySelector('.line-top').classList.toggle('active2');
+  document.querySelector('.line-button').classList.toggle('active3');
+  document.querySelector('.nav-menu').classList.toggle('active4');
+}
